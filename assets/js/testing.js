48c41f2bba8ffdd53ef7587be843f3732bd1cd0c
Nicky Cadavillo <nicoleacadavillo@gmail.com>
2016-03-08 08:11:19 -0500

    api work from last night

--- assets/js/testing.js
+++ assets/js/testing.js
@@ -8,3 +8,23 @@
         this.newChat = ''
       } //closes if
     } //closes sendChat
+
+
+// GIPHY TESTING
+// PUBLIC KEY: dc6zaTOxFJmzC
+// PATH: /v1/gifs/search
+
+
+
+// var xhr = $.get("http://api.giphy.com/v1/gifs/search?&api_key=dc6zaTOxFJmzC&q=" + text);
+//     xhr.done(function(data) {
+//       $("span").html(data.data[0].images.fixed_height.url);
+//     });
+
+
+//WIKIPEDIA
+// $.getJSON("http://en.wikipedia.org/w/api.php?action=query&prop=revisions&rvprop=content&titles="+text+"&format=json&callback=?",
+//   function(data) {
+//     this.chats.push({text: data}),
+//     this.newChat = '';
+// });
